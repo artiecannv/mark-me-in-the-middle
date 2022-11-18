553590deb4d992d8955460df31545c08dd760d2b
loudwhisperer <driftaway44@gmail.com>
2022-11-17 21:03:09 -0700

    added a map from mapbox and functions to control the location and aspect ratio as well as compass direction

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,6 +6,42 @@ const searchBtn = $("#searchbtn");
 const mapKey =
   "pk.eyJ1IjoicG9ya2Nob3BweSIsImEiOiJjbGFraWxzangwNm1rM29vNjBsdDUxaWpiIn0.269b_fLGrm2yTyU3RGPsRw"; //mapbox public key
 
+
+
+
+  navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+  })
+
+  function successLocation(position){
+    console.log(position)
+    setupMap([position.coords.longitude, position.coords.latitude]);
+  }
+
+
+    function errorLocation(){
+      setupMap([-2.24, 53.48])
+    }
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicG9ya2Nob3BweSIsImEiOiJjbGFraWxzangwNm1rM29vNjBsdDUxaWpiIn0.269b_fLGrm2yTyU3RGPsRw";
+function setupMap(center){
+  const map = new mapboxgl.Map({
+    container: "map", 
+    style: "mapbox://styles/mapbox/streets-v11", 
+    center: [-74.5, 40], 
+    zoom: 14, 
+    projection: "globe", 
+  });
+  map.on("style.load", () => {
+    map.setFog({}); 
+  });
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav, "top-right")
+}
+setupMap()
+
+  
+
 function searchManager(event) {
   event.preventDefault()
   const locAVal = locA.val();
